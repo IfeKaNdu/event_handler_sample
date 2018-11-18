1bed33fce9006b48d4377abcdea49098db5b6ffd
IfeKaNdu <softnwa@gmail.com>
2018-11-18 22:03:09 +0000

    changed something

---
+++ event_handler.js
@@ -0,0 +1,14 @@
+
+function myFunction() {
+    var x = document.getElementById("demo");
+    x.style.fontSize = "17px"; 
+    x.style.color = "red"; 
+}
+
+
+function eventHandler(){
+var p = document.getElementById('some-paragraph');
+
+p.innerHTML = 'You clicked me.';
+
+}
